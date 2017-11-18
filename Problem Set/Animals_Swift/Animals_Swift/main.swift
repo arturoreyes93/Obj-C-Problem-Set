bc0ced286c16d261d8f3ee16747e367996bfc9f2
Arturo Reyes <reyesm93@gmail.com>
2017-11-18 13:32:56 -0600

    Fix AnyObject risk use of Obj-C to Swift with optional chaining

--- Problem Set/Animals_Swift/Animals_Swift/main.swift
+++ Problem Set/Animals_Swift/Animals_Swift/main.swift
@@ -23,8 +23,18 @@ let myApartment = Apartment(animals:[sinatra, cary, snoopy])
 let randomNumber = Int(arc4random_uniform(3))
 let farmAnimal = myFarm.animals![randomNumber]
 let cityAnimal = myApartment.animals![randomNumber]
-farmAnimal.scurry()
-cityAnimal.deliverMessage()
+if let scurry = farmAnimal.scurry?() {
+    print("is rat")
+} else {
+    print("is not a rat" )
+}
+
+if let cityAnimal = cityAnimal as? Pigeon {
+    cityAnimal.deliverMessage()
+    print("Pigeon delivering message")
+} else {
+    print("Not a pigeon")
+}
 
 
 
